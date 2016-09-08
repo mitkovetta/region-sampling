4e028f54df1f3e3670292f98610ff4d61ead8feb
Mitko Veta <mitko.veta@gmail.com>
2016-09-08 13:47:12 -0400

    added elastic deformation

--- transform.m
+++ transform.m
@@ -14,15 +14,18 @@ parser = inputParser();
 parser.addRequired('N',         isNni);
 parser.addRequired('channels',  isNni);
 
-parser.addParameter('translation',  0,     isNnn);
-parser.addParameter('rotation',     0,     isAngle);
-parser.addParameter('scaling',      0,     isNnn);
-parser.addParameter('hReflection',  false, isBool);
-parser.addParameter('vReflection',  false, isBool);
-parser.addParameter('channelShift', 0,     isNnn);
-parser.addParameter('contrast',     0,     isNnn);
-parser.addParameter('blur',         0,     isNnn);
-parser.addParameter('whiteNoise',   0,     isNnn);
+parser.addParameter('translation',      0,     isNnn);
+parser.addParameter('rotation',         0,     isAngle);
+parser.addParameter('scaling',          0,     isNnn);
+parser.addParameter('hReflection',      false, isBool);
+parser.addParameter('vReflection',      false, isBool);
+parser.addParameter('channelShift',     0,     isNnn);
+parser.addParameter('intensShift',      0,     isNnn);
+parser.addParameter('contrast',         0,     isNnn);
+parser.addParameter('blur',             0,     isNnn);
+parser.addParameter('whiteNoise',       0,     isNnn);
+parser.addParameter('elasticAlpha',     0,     isNnn);
+parser.addParameter('elasticSigma',     0,     isNnn);
 
 parser.parse(N, channels, varargin{:});
 
@@ -30,18 +33,19 @@ P = parser.Results;
 
 %--------------------------------------------------------------------------
 
-for i_T = N:-1:1
-    
-    T(i_T).translation  = randRange(P.translation * [-1 1], [1 2]);    
-    T(i_T).rotation     = randRange(P.rotation * [-1 1]);    
+for i_T = N:-1:1    
+    T(i_T).translation  = randRange(P.translation * [-1 1], [1 2]);        
+    T(i_T).rotation     = randRange(P.rotation * [-1 1]); 
     T(i_T).scaling      = randRange(P.scaling * [-1 1] + 1);    
     T(i_T).hReflection  = 1 - 2 * randBin() * P.hReflection;
     T(i_T).vReflection  = 1 - 2 * randBin() * P.vReflection;
     T(i_T).channelShift = randRange(P.channelShift * [-1 1], [1 channels]);    
-    T(i_T).contrast     = randRange(P.contrast * [-1 1] + 1);    
-    T(i_T).blur         = randRange([0 P.blur]);    
+    T(i_T).intensShift  = randRange(P.intensShift * [-1 1]);    
+    T(i_T).contrast     = randRange(P.contrast * [-1 1] + 1);   
+    T(i_T).blur         = randRange([0 P.blur]);
     T(i_T).whiteNoise   = randRange([0 P.whiteNoise]);
-    
+    T(i_T).elasticAlpha = P.elasticAlpha;
+    T(i_T).elasticSigma = P.elasticSigma;    
 end
 
 end
